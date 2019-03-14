04e8049cc8d241e2f7d9bc5c5c410d3a077cfbb0
akageun <akageun@gmail.com>
2019-03-14 23:40:59 +0900

    es6 sample

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const tmpValue = "Test Value";
-console.log(`this is ${tmpValue}`);
+((testValue = "default Text") => { //default parameter, arrow function
+
+    console.log(`this is ${testValue}`);
+
+    console.log(testValue);
+
+})("test Text");
